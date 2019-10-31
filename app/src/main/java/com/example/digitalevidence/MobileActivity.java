b8ac329749ec1bc2795f9043425d2e3c5b995c44
Victor Delaplaine <vdelaplaines@aol.com>
2019-10-30 22:59:31 -0700

    the scanner gives 3 items(thats how many are in the db now) but only 1 unique

--- app/src/main/java/com/example/digitalevidence/MobileActivity.java
+++ app/src/main/java/com/example/digitalevidence/MobileActivity.java
@@ -10,6 +10,9 @@ import com.squareup.picasso.Picasso;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.ImageView;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +22,11 @@ import java.util.stream.Collectors;
 
 
 public class MobileActivity extends AppCompatActivity {
+    public static List<String> urlNames; // used for getting the urls
+    public static List<ImageView> imageViews; // used for setting
 
+
+    public static boolean check = true;
     @Override
     @TargetApi(24) // to use java 8
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +35,7 @@ public class MobileActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        List<String> urlNames; // used for getting the urls
-        List<ImageView> imageViews; // used for setting
+
 
         // Step 0 - declare instance of dynamohelper
         DynamoHelper dynamoHelper = new DynamoHelper(this);
@@ -37,29 +43,67 @@ public class MobileActivity extends AppCompatActivity {
         imageViews = getImageViews();
 
         // Step 2 - get all urls from dynamo helper (for mobile objects)
-        dynamoHelper.getAll(MobileDO.class);
+
+       Thread getAll = dynamoHelper.getAll(MobileDO.class);
 
         // Step 3 - Wait while threads are finishing and set urls images to background
-        while(!dynamoHelper.getInThread()){
+        Thread doAll = doAll(dynamoHelper);
 
-            List <MobileDO> mobiles = (List<MobileDO>)dynamoHelper.getModels();
 
-            urlNames = mobiles.stream().map(objToLink()).collect(Collectors.toList());
+        Log.d("1", "try");
+        try {
+            Log.d("2", "entering thread");
+            getAll.start();
 
-            // Step 4 - set all urls to corresponding imageviews
-            setImageViews(urlNames, imageViews);
-            break;
+            Log.d("3", "entering join");
+            getAll.join();
+
+            Log.d("4", "entering do all start");
+            doAll.start();
+
+            Log.d("5", "after do all start");
+
+            Log.d("6", "entering join");
+            doAll.join();
+
+
+        }catch (Exception e){
+            e.getLocalizedMessage();
         }
+
+
+        Log.d("6", "end of main");
+
+    }
+    @TargetApi(24) // to use java 8
+    private Thread doAll (DynamoHelper dynamoHelper){
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List <MobileDO> mobiles = (List<MobileDO>)dynamoHelper.getModels();
+
+                urlNames = mobiles.stream().map(objToLink()).collect(Collectors.toList());
+
+                // Step 4 - set all urls to corresponding imageviews
+                setImageViews(urlNames, imageViews);
+            }
+        });
     }
 
 
     private void setImageViews(List<String> urlNames, List<ImageView> imageViews){
-        ImageView iv;
-        for(int i = 0; i < urlNames.size(); i++) {
-            iv = imageViews.get(i);
-            Picasso.get().load(urlNames.get(i)).into(iv);
+
+        Handler uiHandler = new Handler(Looper.getMainLooper());
+        uiHandler.post(new Runnable(){
+            ImageView iv;
+            @Override
+            public void run() {
+               for(int i = 0; i < urlNames.size(); i++) {
+                    iv = imageViews.get(i);
+                    Picasso.get().load(urlNames.get(i)).into(iv);
+            }
+            }});
         }
-    }
 
     // Description: using lambda function to convert from mobile model to string (link)
     private Function<MobileDO, String> objToLink(){
--- app/src/main/java/com/example/digitalevidence/Models/MobileDO.java
+++ app/src/main/java/com/example/digitalevidence/Models/MobileDO.java
@@ -1,38 +1,8 @@
 package com.example.digitalevidence.Models;
 
-import android.content.Context;
-import android.util.Log;
-
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.config.AWSConfiguration;
-import com.amazonaws.mobileconnectors.dynamodbv2.document.ScanFilter;
-import com.amazonaws.mobileconnectors.dynamodbv2.document.Table;
-import com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Document;
-import com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Primitive;
-import com.amazonaws.mobileconnectors.dynamodbv2.document.internal.KeyDescription;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpression;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.PaginatedList;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ScanRequest;
-import com.example.digitalevidence.Helpers.CredentialHelper;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import static com.example.digitalevidence.Helpers.CredentialHelper.credentialsProvider;
 
 @DynamoDBTable(tableName = "digitalforensics-mobilehub-1666815180-Mobile")
 
@@ -46,10 +16,10 @@ public class MobileDO extends Model{
     public String getName() {
         return _name;
     }
-
     public void setName(final String _name) {
         this._name = _name;
     }
+
     @DynamoDBAttribute(attributeName = "link")
     public static String getLink() {
         return _link;
