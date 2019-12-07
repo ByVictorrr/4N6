9438984da2a1a53fe9de0011557200cbf411db17
Neon <quang.neon.le@gmail.com>
2019-12-07 05:58:57 -0800

    add some files

---
+++ app/src/main/java/com/example/digitalevidence/activities/HelpMobileActivity.java
@@ -0,0 +1,16 @@
+package com.example.digitalevidence.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.digitalevidence.R;
+
+public class HelpMobileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_mobile);
+    }
+}
