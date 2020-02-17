acb3f7d54c023494aefd1ad95afb2108ba34655a
Victor Delaplaine <vdelaplaines@aol.com>
2020-02-17 14:45:13 -0800

    amplify add

--- app/src/main/java/com/example/digitalevidence/activities/ProfileActivity.java
+++ app/src/main/java/com/example/digitalevidence/activities/ProfileActivity.java
@@ -17,60 +17,27 @@ public class ProfileActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.toolbar_profile);
+        final String TAG="auth error";
 
         // Toolbar
         TextView textView = findViewById(R.id.toolbar_title);
         textView.setText(R.string.title_profile);
 
-        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
-            @Override
-            public void onResult(UserStateDetails userStateDetails) {
-                switch (userStateDetails.getUserState()) {
-                    case SIGNED_IN:
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                TextView textView = (TextView) findViewById(R.id.test);
-                                textView.setText("Logged IN");
-                            }
-                        });
-                        break;
-                    case SIGNED_OUT:
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                TextView textView = (TextView) findViewById(R.id.test);
-                                textView.setText("Logged OUT");
-                            }
-                        });
-                        break;
-                    default:
-                        AWSMobileClient.getInstance().signOut();
-                        break;
-                }
-            }
-
-            @Override
-            public void onError(Exception e) {
-                Log.e("INIT", e.toString());
-            }
-        });
-
-        AWSMobileClient.getInstance().showSignIn(this, SignInUIOptions.builder()
-            .nextActivity(MobileActivity.class)
-            .backgroundColor(R.color.colorBackground)
-            .canCancel(true)
-            .build(),
+        AWSMobileClient.getInstance().showSignIn(
+                this,
+                SignInUIOptions.builder()
+                        .nextActivity(MainActivity.class)
+                        .build(),
                 new Callback<UserStateDetails>() {
                     @Override
                     public void onResult(UserStateDetails result) {
-                        Log.d("Test", "onResult: " + result.getUserState());
+                        Log.d(TAG, "onResult: " + result.getUserState());
                         switch (result.getUserState()){
                             case SIGNED_IN:
                                 Log.i("INIT", "logged in!");
                                 break;
                             case SIGNED_OUT:
-                                Log.i("Test", "onResult: User did not choose to sign-in");
+                                Log.i(TAG, "onResult: User did not choose to sign-in");
                                 break;
                             default:
                                 AWSMobileClient.getInstance().signOut();
@@ -80,9 +47,11 @@ public class ProfileActivity extends BaseActivity {
 
                     @Override
                     public void onError(Exception e) {
-                        Log.e("TestError", "onError: ", e);
+                        Log.e(TAG, "onError: ", e);
                     }
                 }
         );
+
+
     }
 }
